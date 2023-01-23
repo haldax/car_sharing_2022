dc41359678d7779faa2224d9c62f477714b1db2c
Sofie502 <121302938+Sofie502@users.noreply.github.com>
2023-01-23 16:51:02 +0100

    Update tracking.js

--- script/tracking.js
+++ script/tracking.js
@@ -5,6 +5,30 @@ var totalHours = 0;
 var totalMinutes = 0;
 var totalSeconds = 0;
 
+var buttonClicks = 0;
+var userK = 0;
+var userC = 0;
+var emailK = 0;
+var emailC = 0;
+var pwK = 0;
+var pwC = 0;
+var fnameK = 0;
+var fnameC = 0;
+var lnameK = 0;
+var lnameC = 0;
+var addressK = 0;
+var addressC = 0;
+var countryK = 0;
+var countryC = 0;
+var zipcodeK = 0;
+var zipcodeC = 0;
+var sexK = 0;
+var sexC = 0;
+var languageK = 0;
+var languageC = 0;
+var aboutK = 0;
+var aboutC = 0;
+
 //track the mouse clicks
 document.onclick = function(){
     clicks++;
@@ -12,7 +36,7 @@ document.onclick = function(){
 }
 
 //track total key presses
-document.onkeydown = function(){
+document.onkeyup = function(){
     totalKeys++;
     document.getElementById("track-total-keys").innerHTML = "Total key presses: " + totalKeys;
 }
@@ -50,25 +74,66 @@ function totalTime(){
 
 //count total characters typed
 function totalCharacters(){
-    var user = document.getElementById("user").value.length;
-    var email = document.getElementById("email").value.length;
-    var pw = document.getElementById("pw").value.length;
-    var fname = document.getElementById("fname").value.length;
-    var lname = document.getElementById("lname").value.length;
-    var address = document.getElementById("address").value.length;
-    var country = document.getElementById("country").value.length;
-    var zipcode = document.getElementById("zipcode").value.length;
+    userC = document.getElementById("user").value.length;
+    emailC = document.getElementById("email").value.length;
+    pwC = document.getElementById("pw").value.length;
+    fnameC = document.getElementById("fname").value.length;
+    lnameC = document.getElementById("lname").value.length;
+    addressC = document.getElementById("address").value.length;
+    countryC = document.getElementById("country").value.length;
+    zipcodeC = document.getElementById("zipcode").value.length;
     var about = document.getElementById("about").value;
 
     //exclude the text in the optional box if nothing has been written by the user
     if (about.includes("(Optional)Say something about yourself..")){
-        about = 0;
+        aboutC = 0;
     } else{
-        about = about.length;
+        aboutC = about.length;
     }
 
-    var totalChar = user + email + pw + fname + lname + address + country + zipcode + about;
+    var totalChar = userC + emailC + pwC + fnameC + lnameC + addressC + countryC + zipcodeC + aboutC;
     document.getElementById("track-total-chars").innerHTML = "Total number of characters typed: " + totalChar;
+}
+
+//count the number of key presses per field
+function addKey(id){
+    document.getElementById(id).onkeyup = function(){
+        switch(id){
+            case "user":
+                userK++;
+                break;
+            case "email":
+                emailK++;
+                break;
+            case "pw":
+                pwK++;
+                break;
+            case "fname":
+                fnameK++;
+                break;
+            case "lname":
+                lnameK++;
+                break;
+            case "address":
+                addressK++;
+                break;
+            case "country":
+                countryK++;
+                break;
+            case "zipcode":
+                zipcodeK++;
+                break;
+            case "sex":
+                sexK++;
+                break;
+            case "language":
+                languageK++;
+                break;
+            case "about":
+                aboutK++;
+                break;
+        }
+    }
 }
 
 //make the hidden div visible
@@ -81,5 +146,34 @@ function showDiv(e){
 
 //make the hidden div with the table visible
 function showFields(){
-    document.getElementById("hidden-div-fields").style.display = "block";
+    buttonClicks++;         //to determine if the table should be visible
+
+    if((buttonClicks % 2) == 0){
+        document.getElementById("hidden-div-fields").style.display = "none";
+    } else{
+        document.getElementById("userK").innerHTML = userK;
+        document.getElementById("userC").innerHTML = userC;
+        document.getElementById("emailK").innerHTML = emailK;
+        document.getElementById("emailC").innerHTML = emailC;
+        document.getElementById("pwK").innerHTML = pwK;
+        document.getElementById("pwC").innerHTML = pwC;
+        document.getElementById("fnameK").innerHTML = fnameK;
+        document.getElementById("fnameC").innerHTML = fnameC;
+        document.getElementById("lnameK").innerHTML = lnameK;
+        document.getElementById("lnameC").innerHTML = lnameC;
+        document.getElementById("addressK").innerHTML = addressK;
+        document.getElementById("addressC").innerHTML = addressC;
+        document.getElementById("countryK").innerHTML = countryK;
+        document.getElementById("countryC").innerHTML = countryC;
+        document.getElementById("zipcodeK").innerHTML = zipcodeK;
+        document.getElementById("zipcodeC").innerHTML = zipcodeC;
+        document.getElementById("sexK").innerHTML = sexK;
+        document.getElementById("sexC").innerHTML = sexC;
+        document.getElementById("languageK").innerHTML = languageK;
+        document.getElementById("languageC").innerHTML = languageC;
+        document.getElementById("aboutK").innerHTML = aboutK;
+        document.getElementById("aboutC").innerHTML = aboutC;
+
+        document.getElementById("hidden-div-fields").style.display = "block";
+    }
 }
